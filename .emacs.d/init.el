3007634437524fa7b1af46167b9f6b5267483dbb
RepulsiveChap <joel.ardarve@gmail.com>
2016-06-23 18:53:02 +0200

    Lots of changes to the emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,19 @@
-;;; What a great config
+;;; Repulsive Chaps emacs config
+;;; Adds an archive of packages to install
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+;;; Sets altgr+e to change inside "something"
+(global-set-key (kbd "€") 'er/expand-region)
+;;; Adds alt+x to be like d menu
+(global-set-key (kbd "M-x") 'smex)
+;;; Adds ctl+x ctrl+f to act like d menu
+(ido-mode 1)
+;;;Configs to remove the gui from gui emacs
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(fringe-mode 0)
+;;;Changes the white background to black and black text to white
+(add-to-list 'default-frame-alist '(foreground-color . "#ffffff"))
+(add-to-list 'default-frame-alist '(background-color . "#000000"))
