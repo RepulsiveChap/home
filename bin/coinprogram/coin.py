5d629e304f15507389d575758e191eb19e40e6de
Joel Ardarve <saft@strawberry>
2017-11-30 00:42:35 +0100

    Comments and sum in sek

--- bin/coinprogram/coin.py
+++ bin/coinprogram/coin.py
@@ -16,30 +16,64 @@ wallet = Wallet()
 worth = wallet.calculateWorth(parser)
 expences = Expences()
 dollar = 8.3
-# print(worth)
 
 
 
 
-#Parsing the sysargs
+#One argument
 if len(sys.argv) == 2:
+
+    #Wallet, returns wallet value in dollars
     if(sys.argv[1] == "wallet"):
         worth = wallet.calculateWorth(parser)
         print(worth)
+
+    #Edits the wallet
     elif(sys.argv[1] == "e"):
         wallet.editWallet()
+
+    #Prints the expences in sek
     elif(sys.argv[1] == "expences"):
         print(expences.calculateExpences())
-    elif(sys.argv[1] == "sum"):
-        sumExpences = expences.calculateExpences()
-        worth = wallet.calculateWorth(parser)
-        worth = worth * dollar
-        print(worth - sumExpences)
+
+    #Subtracts the expences from the value. Prints in sek
+            
+
+    #Prints the value of the given coin in dollars
     else:
         parser.printValue(sys.argv[1] + "/")
+
+#Two or morearguments
 if len(sys.argv) > 2:
     if sys.argv[1] == 'pvalue':
         parser.printValue(sys.argv[2] + "/")
+
+    elif(sys.argv[1] == "sum"):
+        if(sys.argv[2] == "sek"):
+            sumExpences = expences.calculateExpences()
+            worth = wallet.calculateWorth(parser)
+            worth = worth * dollar
+            print(worth - sumExpences)
+        elif(sys.argv[2] == "dollar"):
+            sumExpences = expences.calculateExpences()
+            sumExpences = sumExpences / dollar
+            worth = wallet.calculateWorth(parser)
+            print(worth - sumExpences)
+    elif(sys.argv[1] == "wallet"):
+        if(sys.argv[2] == "sek"):
+            worth = wallet.calculateWorth(parser)
+            worth = worth * dollar
+            print(worth)
+        elif(sys.argv[2] == "dollar"):
+            worth = wallet.calculateWorth(parser)
+            print(worth)
+            pass
+        else:
+            print("Please enter valid currency")
+
+    else:
+        print("Please enter valid currency")
+
 if len(sys.argv) == 1:
     print("Give the program a few arguments")
     print("Example: coin pvalue bitcoin")
